025f2956e6203212f4cd9357b835dd491f04d5b9
Diseño <unila@iMac-de-Diseno.local>
2019-11-25 18:09:56 -0600

    Master

--- header.php
+++ header.php
@@ -11,8 +11,6 @@
 <!-- Maximize -->
     <link rel="stylesheet" href="css/maximize.css">
 <!-- Style -->
-    <link rel="stylesheet" href="css/layout.css">
-<!-- Header -->
     <link rel="stylesheet" href="css/header.css">
 <!-- @media 414 CSS-->
     <link rel="stylesheet" href="css/@media414.css">
@@ -26,13 +24,26 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet"> 
 <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/df16b521b5.js" crossorigin="anonymous"></script>
-    <title>layout</title>
+    <title>header</title>
 </head>
-<body>	
-<header class="col-xl-12">
-    <iframe src="header.html" frameborder="0" width="100%"  height="500"></iframe>
-</header>
+<body> 
 
+<header>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Features</a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    </div>
+  </div>
+</nav>
+</header>
 
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -42,6 +53,6 @@
 <!-- Bootstrap JS -->
     <script src="js/bootstrap.js"></script>
 <!-- Maximize JS -->
-<script src="../js/maximize.js"></script>
+    <script src="../js/maximize.js"></script>
 </body>
 </html>
